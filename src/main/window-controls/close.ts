b097a5183a8c109fc6e7226185f804c7ed203bed
ahmed-abd-alalim <ahmedabdalalim.3a@gmail.com>
2025-04-25 06:38:05 +0300

    #

--- src/main/window-controls/close.ts
+++ src/main/window-controls/close.ts
@@ -4,17 +4,11 @@ export default (): void => {
   ipcMain.on('close-window', (event) => {
     const window = BrowserWindow.fromWebContents(event.sender)
     // @ts-ignore (define in dts)
-    // if (window.name === 'main') {
-    //   BrowserWindow.getAllWindows().forEach((win) => win.close())
-    // } else {
-
-    BrowserWindow.getAllWindows().forEach((win) => {
-      // if (win.name === 'workspace') {
-      //   win?.webContents.send('session-not-active', true)
-      // }
-    })
-
-    // window?.close()
-    // }
+    if (window.name === 'main') {
+      BrowserWindow.getAllWindows().forEach((win) => win.close())
+    } else {
+      window?.webContents.send('session-not-active', true)
+      window?.close()
+    }
   })
 }
