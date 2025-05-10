8eba4d56996e40cf98585368a5034082f6b49b2e
ahmed-abd-alalim <ahmedabdalalim.3a@gmail.com>
2025-05-10 15:51:45 +0300

    bg for routingPanel page

--- src/renderer/src/workspace/pages/routing-panel/index.tsx
+++ src/renderer/src/workspace/pages/routing-panel/index.tsx
@@ -1,7 +1,11 @@
 import './routingPanel.css'
 
 const Index = (): React.JSX.Element => {
-  return <div></div>
+  return (
+    <section id="routingPanel">
+      <div className="bg_mask" />
+    </section>
+  )
 }
 
 export default Index
